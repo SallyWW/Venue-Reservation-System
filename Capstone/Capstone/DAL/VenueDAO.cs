160c20336b60ea9110928bfd3814f5844337983b
Dan Schlegel <danielpschlegel@gmail.com>
2021-06-17 11:37:23 -0400

    capstone2

--- Capstone/Capstone/DAL/VenueDAO.cs
+++ Capstone/Capstone/DAL/VenueDAO.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Capstone.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace Capstone.DAL
@@ -10,10 +12,44 @@ namespace Capstone.DAL
     public class VenueDAO
     {
         private readonly string connectionString;
+        private const string SqlSelectAllVenues = "SELECT id, name, city_id, description FROM venue ORDER BY name";
 
-        public VenueDAO (string connectionString)
+        public VenueDAO(string connectionString)
         {
             this.connectionString = connectionString;
         }
+
+        public IList<Venue> GetAllVenues()
+        {
+            List<Venue> venues = new List<Venue>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand command = new SqlCommand(SqlSelectAllVenues, conn);
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Venue venue = new Venue();
+                        venue.Id = Convert.ToInt32(reader["id"]);
+                        venue.Name = Convert.ToString(reader["name"]);
+                        venue.CityId = Convert.ToInt32(reader["city_id"]);
+                        venue.Description = Convert.ToString(reader["description"]);
+                        venues.Add(venue);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Problem getting venues: " + ex.Message);
+            }
+
+            
+            return venues;
+        }
     }
+
 }
--- Capstone/Capstone/UserInterface.cs
+++ Capstone/Capstone/UserInterface.cs
@@ -1,4 +1,5 @@
 ﻿using Capstone.DAL;
+using Capstone.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -31,10 +32,95 @@ namespace Capstone
             venueDAO = new VenueDAO(connectionString);
         }
 
+        //private VenueDAO venuedao = new VenueDAO();
+
+        private List<int> menuWalkBack = new List<int>();
         public void Run()
         {
-            Console.WriteLine("Reached the User Interface.");
-            Console.ReadLine();
+            menuWalkBack = new List<int>();
+            menuWalkBack.Add(1);
+            bool mainMenu = true;
+            while (mainMenu)
+            {
+
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("1. List Venues");
+                Console.WriteLine("2. Quit");
+                string userInput = Console.ReadLine().ToString();
+                if (userInput == "1")
+                {
+                    //method();
+                }
+                if (userInput == "2")
+                {
+                    mainMenu = false;
+                }
+                else
+                {
+                    Console.WriteLine("Please Enter a Valid Choice");
+                    continue;
+                }
+            }
+
+
+        }
+        public void ListVenues()
+        {
+
+            //IList<Venue> venueList = VenueDAO.GetAllVenues();
+
+            menuWalkBack.Add(2);
+            // dao.DisplayAllVenues();
+
+
+            //$"Venue is called {venue.Name} and its description is {venue.Description} and..."
+        }
+
+
+
+
+
+
+
+
+        public void ReturnToPrevious()
+        {
+            // remove last entry in list
+            int lastItem = menuWalkBack.Count - 1;
+            menuWalkBack.RemoveAt(lastItem);
+
+            //  go to last item in list
+            var lastIndex = menuWalkBack.Count - 1;
+            MenuSwitch(menuWalkBack[lastIndex]);
+        }
+        public void MenuSwitch(int menu)
+        {
+            // remove the current index we're using to go to previous
+            if (menuWalkBack.Count > 1)
+            {
+                int lastIndex = menuWalkBack.Count - 1;
+                menuWalkBack.RemoveAt(lastIndex);
+
+            }
+            switch (menu)
+            {
+                case 1:
+                    Run();
+                    break;
+                    //case 2:
+                    //    UserSelectVenue();
+                    //    break;
+                    //case 3:
+                    //    ThirdMenu();
+                    //    break;
+                    ////case 4:
+                    ////    FourthMenu();
+                    ////    break;
+                    //default:
+                    //    MainMenu();
+                    //    break;
+
+            }
         }
     }
 }
