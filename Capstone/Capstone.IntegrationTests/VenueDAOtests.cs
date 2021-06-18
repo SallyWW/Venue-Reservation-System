9d4644fdd69d758d68ace01a33a456caf3372ec4
Dan Schlegel <danielpschlegel@gmail.com>
2021-06-18 18:32:28 -0400

    cap2

---
+++ Capstone/Capstone.IntegrationTests/VenueDAOtests.cs
@@ -0,0 +1,51 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capstone.DAL;
+using Capstone.Models;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+
+
+namespace Capstone.IntegrationTests
+{
+    [TestClass]
+    public class VenueDAOtests : IntegrationTestBase
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
